f845f180b2379da1b9e676e6457bc5b7ac23e782
Tech Hacker <114179175+TechHacker737@users.noreply.github.com>
2023-07-08 14:55:09 +0530

    Update wspam.py

--- wspam.py
+++ wspam.py
@@ -212,15 +212,15 @@ def logo():
 	auth=m+'  Creat By : '+k+'Tech Hacker | Subscribe: youtube.com/techhacker7'
 	# jika ingin m3namambah kan variabel dan mengubah data variabel kitsune bisa menambahkan %s menambahkan variabel terus di ubah menjjadu string, %d = mengubah data menjadi decimal , %i = mengubah data menjadi integer
 	return '''
-%s      .-"-.            .-"-.         %s╲╲╲╲╲╲%s
-%s   _ /_-.-_\_         /.-.-.\_       %s╲╲╲╲╲╲%s
-%s   / __} {__ \      /|( o o )|\      %s╲╲%s ╦ ╦╦ ╦╔═╗╔╦╗╔═╗╔═╗╔═╗╔═╗
-%s  / //  "  \\ \    | //  "  \\ |     %s╲%s    ║║║╠═╣╠═╣ ║ ╚═╗╠═╣╠═╝╠═╝
-%s / / \'---'/ \ \  / / \'---'/ \ \    %s╲%s    ╚╩╝╩ ╩╩ ╩ ╩ ╚═╝╩ ╩╩  ╩ 
-%s \ \_/`"""`\_/ /  \ \_/`"""`\_/ /    %s╲%s  ╔═╗╔═╗╔═╗╔╦╗
-%s  \           /    \           /     %s╲╲%s ╚═╗╠═╝╠═╣║║║
-%s                                     %s╲╲%s ╚═╝╩  ╩ ╩╩ ╩
-%s                                     %s╲╲%s
+%s      .-"-.            .-"-.            %s╲╲╲╲╲╲%s
+%s   _ /_-.-_\_         /.-.-.\_          %s╲╲╲╲╲╲%s
+%s   / __} {__ \      /|( o o )|\         %s╲╲%s ╦ ╦╦ ╦╔═╗╔╦╗╔═╗╔═╗╔═╗╔═╗
+%s  / //  "  \\ \    | //  "  \\ |        %s╲%s    ║║║╠═╣╠═╣ ║ ╚═╗╠═╣╠═╝╠═╝
+%s / / \'---'/ \ \  / / \'---'/ \ \       %s╲%s    ╚╩╝╩ ╩╩ ╩ ╩ ╚═╝╩ ╩╩  ╩ 
+%s \ \_/`"""`\_/ /  \ \_/`"""`\_/ /       %s╲%s  ╔═╗╔═╗╔═╗╔╦╗
+%s  \           /    \           /        %s╲╲%s ╚═╗╠═╝╠═╣║║║
+%s                                        %s╲╲%s ╚═╝╩  ╩ ╩╩ ╩
+%s                                        %s╲╲%s
 %s''' % (k,m,h,k,m,h,k,m,h,k,m,h,k,m,h,k,m,h,k,m,h,k,m,h,k,m,h,auth)
 # -----------------------------------------------------------
 def termux():
